434a40e60e359cf7201aef8d51000bd2ecc74488
torresdavi <davitorres@cjr.org.br>
2018-04-04 09:06:06 -0300

    Criei a tabela do backoffice e fiz a paginação, porém não sei alterar o nome. Está com os links: next e last e o número de páginas está junto, nao tem espaçamento. (EX: tem 2 paginas, dai ao inves de ficar: 1 2 Next Last, ficou: 12Next>Last>>.

--- app/controllers/backoffice_controller.rb
+++ app/controllers/backoffice_controller.rb
@@ -3,6 +3,8 @@ class BackofficeController < ApplicationController
 	before_action :authenticate_admin!
 
 	def index
+		@humans = Human.all
+		@humans = Human.page(params[:page]).per(2)
 	end
 	
 end
