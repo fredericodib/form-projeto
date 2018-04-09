e60026fc7323fc0db541ca60befc96516d12f492
torresdavi <davitorres@cjr.org.br>
2018-04-09 17:36:30 -0300

    Pagina do backoffice tabelada e paginada funcionando normalmente. Campo de filtro para país funcionando também. Na view humans new.html.erb coloquei o dropdown para selecionar o país.

--- app/controllers/backoffice_controller.rb
+++ app/controllers/backoffice_controller.rb
@@ -3,8 +3,10 @@ class BackofficeController < ApplicationController
 	before_action :authenticate_admin!
 
 	def index
-		@humans = Human.all
-		@humans = Human.page(params[:page]).per(10)
-	end
-	
+		if (params[:search] && params[:search] != "")
+			@humans = Human.where(country: params[:search]).page(params[:page]).per(60)
+		else
+			@humans = Human.page(params[:page]).per(60)
+		end
+	end	
 end
