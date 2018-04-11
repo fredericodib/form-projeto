9526199a95faae3460b4f81b9eb88a0a31074dbd
PedrosUsername <pedrorodrigues@cjr.org.br>
2018-04-11 10:26:21 -0300

    remover linguageneses

--- app/controllers/humans_controller.rb
+++ app/controllers/humans_controller.rb
@@ -1,34 +1,6 @@
 class HumansController < ApplicationController
 
-  def new
-    @language = 0
-
-
-
-    if @language == 0
-      @titulo = "Pesquisa Musical"
-      @inicial_text = "Por favor, seja sincero com as respostas, e responda este formulário apenas uma vez"
-      @seu_nome = "nome"
-      @seu_pais = "país"
-      @seu_conhecimento = "possui algum conhecimento musical?"
-      @sim = "sim"
-      @nao = "não"
-      @tocaOUNtoca = "você toca algum instrumento?"
-      @reordena = "ordene os fragmentos abaixo como desejar.";
-      @enviar = "enviar"
-    elsif @language == 1
-      @titulo = "Musical Research"
-      @inicial_text = "Please, be sincere in your answers, and answer this formulary only once"
-      @seu_nome = "name"
-      @seu_pais = "country"
-      @seu_conhecimento = "do you have any musical background?"
-      @sim = "yes"
-      @nao = "no"
-      @tocaOUNtoca = "do you play any instrument?"
-      @reordena = "sort the audio frags below as you wish.";
-      @enviar = "submit"
-    end
-  
+  def new  
     @human = Human.new
   end
 
